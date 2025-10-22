8fa96c06ca65e1a5a380ae7d59d1c9fe7bff00e9
Abdulaziz-Ali123 <abdulazizali476@gmail.com>
2025-10-22 13:23:20 -0500

    Initial commit of starter template for LocalNotes

---
+++ main/background.ts
@@ -0,0 +1,185 @@
+import path from 'path'
+import { app, ipcMain, Menu, dialog } from 'electron'
+import serve from 'electron-serve'
+import { createWindow } from './helpers'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+// if current os is a macbookd
+const isMac = process.platform === 'darwin'
+
+if (isProd) {
+  serve({ directory: 'app' })
+} else {
+  app.setPath('userData', `${app.getPath('userData')} (development)`)
+}
+
+(async () => {
+  await app.whenReady()
+
+  const mainWindow = createWindow('main', {
+    width: 1000,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    titleBarStyle: 'hidden',
+  })
+
+  // for context menu the one that pops up when you right click
+  const contextTemplate:any = [
+    { role: 'copy' },
+    { role: 'cut' },
+    { role: 'paste' },
+    { role: 'selectall' }
+  ]
+
+  //for menubar
+  const template:any = [
+    // { role: 'appMenu' }
+    ...(isMac
+      ? [{
+          label: app.name,
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideOthers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+          ]
+        }]
+      : []),
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      submenu: [
+        isMac ? { role: 'close' } : { role: 'quit' },
+        {
+          //open files
+          label: 'Open File...',
+          click: () => dialog.showMessageBox({message: 'Opening file...'}),
+          accelerator: 'CommandOrControl+O'
+
+        },
+        {
+          //open folder
+          label: 'Open Folder...'
+        }
+      ]
+    },
+    // { role: 'editMenu' }
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac
+          ? [
+              { role: 'pasteAndMatchStyle' },
+              { role: 'delete' },
+              { role: 'selectAll' },
+              { type: 'separator' },
+              {
+                label: 'Speech',
+                submenu: [
+                  { role: 'startSpeaking' },
+                  { role: 'stopSpeaking' }
+                ]
+              }
+            ]
+          : [
+              { role: 'delete' },
+              { type: 'separator' },
+              { role: 'selectAll' }
+            ])
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac
+          ? [
+              { type: 'separator' },
+              { role: 'front' },
+              { type: 'separator' },
+              { role: 'window' }
+            ]
+          : [
+              { role: 'close' }
+            ])
+      ]
+    },
+    {
+      // Open/Close developer tools
+      label: 'Toggle Developer Tools',
+      click: () => mainWindow.webContents.toggleDevTools(),
+      accelerater: isMac ? 'Command+Option+I' : 'Control+Shift+I' 
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: async () => {
+            const { shell } = require('electron')
+            await shell.openExternal('https://electronjs.org')
+          }
+        }
+      ]
+    }
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
+  const contextMenu = Menu.buildFromTemplate(contextTemplate)
+
+  mainWindow.webContents.on('context-menu', (_event, params) => {
+    if (params.isEditable){
+      contextMenu.popup()
+    }
+  })
+
+  if (isProd) {
+    await mainWindow.loadURL('app://./home')
+  } else {
+    const port = process.argv[2]
+    await mainWindow.loadURL(`http://localhost:${port}/home`)
+  }
+
+})()
+
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+ipcMain.on('message', async (event, arg) => {
+  event.reply('message', `${arg} World!`)
+})
---
+++ renderer/pages/editor.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/renderer/components/ui/resizable"
+
+export default function Editor() {
+  return (
+    <React.Fragment>
+      <ResizablePanelGroup
+      direction="horizontal"
+      className="min-h-screen w-full border-none"
+      >
+      <ResizablePanel defaultSize={25}>
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">File Tree</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle className='bg-transparent'/>
+      <ResizablePanel defaultSize={75} minSize={60}>
+        <div className="flex h-full items-center justify-center p-6 rounded-3xl bg-secondary">
+          <span className="font-semibold">Text Editor</span>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+      
+
+    </React.Fragment>
+  )
+}
