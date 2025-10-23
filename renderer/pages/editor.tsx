4e83c39d97425840d22cfb44a86d00b0d17094a0
Shaun <ssubat0628@gmail.com>
2025-10-23 18:50:35 -0500

    sperated it into a component

--- renderer/pages/editor.tsx
+++ renderer/pages/editor.tsx
@@ -4,115 +4,9 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/renderer/components/ui/resizable";
-import { File, Folder, Tree } from "@/renderer/components/ui/file-tree";
-
-const ELEMENTS = [
-  {
-    id: "1",
-    isSelectable: true,
-    name: "src",
-    children: [
-      {
-        id: "2",
-        isSelectable: true,
-        name: "app",
-        children: [
-          {
-            id: "3",
-            isSelectable: true,
-            name: "layout.tsx",
-          },
-          {
-            id: "4",
-            isSelectable: true,
-            name: "page.tsx",
-          },
-        ],
-      },
-      {
-        id: "5",
-        isSelectable: true,
-        name: "components",
-        children: [
-          {
-            id: "6",
-            isSelectable: true,
-            name: "header.tsx",
-          },
-          {
-            id: "7",
-            isSelectable: true,
-            name: "footer.tsx",
-          },
-        ],
-      },
-      {
-        id: "8",
-        isSelectable: true,
-        name: "lib",
-        children: [
-          {
-            id: "9",
-            isSelectable: true,
-            name: "utils.ts",
-          },
-        ],
-      },
-    ],
-  },
-];
-
-export function FileTreeDemo() {
-  return (
-    <Tree
-      className="bg-background overflow-hidden rounded-md p-2"
-      initialSelectedId="7"
-      initialExpandedItems={[
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "11",
-      ]}
-      elements={ELEMENTS}
-    >
-      <Folder element="src" value="1">
-        <Folder value="2" element="app">
-          <File value="3">
-            <p>layout.tsx</p>
-          </File>
-          <File value="4">
-            <p>page.tsx</p>
-          </File>
-        </Folder>
-        <Folder value="5" element="components">
-          <Folder value="6" element="ui">
-            <File value="7">
-              <p>button.tsx</p>
-            </File>
-          </Folder>
-          <File value="8">
-            <p>header.tsx</p>
-          </File>
-          <File value="9">
-            <p>footer.tsx</p>
-          </File>
-        </Folder>
-        <Folder value="10" element="lib">
-          <File value="11">
-            <p>utils.ts</p>
-          </File>
-        </Folder>
-      </Folder>
-    </Tree>
-  );
-}
+import { FileTree } from "@/renderer/components/filetree";
+import { Button } from "@/renderer/components/ui/button";
+import { FolderPlusIcon } from "lucide-react";
 
 export default function Editor() {
   return (
@@ -123,7 +17,14 @@ export default function Editor() {
       >
         <ResizablePanel defaultSize={25}>
           <div className="flex h-full items-center justify-center p-6">
-            <FileTreeDemo />
+            <div className="w-full h-full">
+              <div className="flex items-center gap-2">
+                <Button variant="outline" size="icon" className="w-fit h-fit">
+                  <FolderPlusIcon className="size-4" />
+                </Button>
+              </div>
+              <FileTree />
+            </div>
           </div>
         </ResizablePanel>
         <ResizableHandle withHandle className="bg-transparent" />
