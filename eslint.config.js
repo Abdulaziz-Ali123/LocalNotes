fe0c1aae7f2bdab7ebb3ff6e61f412c1bf7279ea
Malek Kchaou <malek.git2001@gmail.com>
2025-11-08 23:34:52 -0600

    added "type"="module" attribute in package.json for eslint parsing reasons added eslint.config.js file needed for the new npm run lint command

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+﻿import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            parser,
+            parserOptions: {
+                project: "./tsconfig.json",
+                ecmaFeatures: { jsx: true },
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tseslint,
+            react,
+        },
+        rules: {
+            ...tseslint.configs.recommended.rules,
+            "react/react-in-jsx-scope": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+        },
+    },
+];
+
+
