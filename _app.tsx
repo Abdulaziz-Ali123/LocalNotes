f7dffe26c7e609ece8edfcd07387bb08f725d2cc
a157p624 <157836241+a157p624@users.noreply.github.com>
2025-11-09 23:14:15 -0600

    Implement LaTeX rendering in notes

---
+++ _app.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import type { AppProps } from 'next/app'
+
+import '../styles/globals.css'
+import 'katex/dist/katex.min.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <div className='h-screen w-full overflow-hidden'>
+        <Component {...pageProps} />
+      </div>
+   )
+}
+
+export default MyApp
