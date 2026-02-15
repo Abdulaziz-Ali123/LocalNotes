d9f3a6fac717f3660027a7eab391d78805e67732
Abdulaziz-Ali123 <abdulazizali476@gmail.com>
2026-02-15 14:00:25 -0600

    added function to get config folde path

--- main/helpers/config-dir.ts
+++ main/helpers/config-dir.ts
@@ -4,6 +4,7 @@ import os from "os";
 import path from "path";
 
 const APP_CONFIG_DIR_NAME = "LocalNotes";
+let CONFIG_PATH_CREATED = false
 
 const getBaseDataDirectory = (): string => {
 	if (process.platform === "win32") {
@@ -55,7 +56,12 @@ export const ensureConfigDirectory = async (): Promise<string> => {
 		console.log(statusMessage);
 	} else {
 		console.log(statusMessage);
+		CONFIG_PATH_CREATED = true
 	}
 
 	return configDirectoryPath;
 };
+
+export const getConfigDirectoryPath = (): string => {
+	return getBaseDataDirectory() + "/" +APP_CONFIG_DIR_NAME
+}
