79bb3fa9058241ea7a4ae10fe70fccf69395fd49
Malek Kchaou <malek.git2001@gmail.com>
2026-03-28 17:35:14 -0500

    a couple more changes with regards to error-logging

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/renderer/lib/theme";
 import { useBoundStore } from "@/renderer/store/useBoundStore";
 
 import 'katex/dist/katex.min.css'
+import { ErrorToastProvider } from "@/renderer/components/feedback/ErrorToastProvider";
 
 // App Component
 
@@ -18,10 +19,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [initializeSettings]);
 
   return (
-    <ThemeProvider>
-      <div className="h-screen w-full">
-        <Component {...pageProps} />
-      </div>
+      <ThemeProvider>
+          <ErrorToastProvider>
+              <div className="h-screen w-full">
+                <Component {...pageProps} />
+              </div>
+          </ErrorToastProvider>
     </ThemeProvider>
   );
 }
